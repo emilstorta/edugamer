39d547be726655ed7a9dd60785ef8c6e9917d8b3
Kristoffer <Kristoffer.sg123@gmail.com>
2019-12-03 11:59:11 +0100

    Mysql update

--- authenicate.php
+++ authenicate.php
@@ -15,20 +15,19 @@ if (!isset($_POST['username'], $_POST['password'])){
     die ('Udfyld venligst både brugernavn og kodeord');
 }
 
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+if ($stmt = $con->prepare('SELECT password FROM accounts WHERE username = ?')) {
     $stmt->bind_param('s', $_POST['username']);
     $stmt->execute();
     $stmt->store_result();
 } 
 if ($stmt->num_rows > 0) {
-    $stmt->bind_result($id, $password);
+    $stmt->bind_result( $password);
     $stmt->fetch();
 
     if ($_POST['password'] == $password) {
         session_regenerate_id();
         $_SESSION['loggedin'] = TRUE;
         $_SESSION['name'] = $_POST['username'];
-        $_SESSION['id'] = $id;
         header('Location: home.php');
     } 
     else {
